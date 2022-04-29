b2159992f06b97503a21afc8d52f07d1d4c29b5d
IgorFollador <igor.follador@compasso.com.br>
2022-04-29 16:55:50 -0300

    Simplifying RegEx

--- extrator_cep.py
+++ extrator_cep.py
@@ -3,7 +3,7 @@ endereco = "Avenida Adão Welker 81, Centro, Barão de Cotegipe, RS, 99740-000"
 import re # Regular Expression --RegEx
 
 # 5 dígitos + hífen(opcional) + 3 dígitos
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]?[0-9]{3}")
 busca = padrao.search(endereco) # Match
 if busca:
     cep = busca.group()
